3e0c6397f8b5a41c2cb401990a28b3402d57c628
Jordan Walker <jordan@jordanwalker.us>
2021-03-10 20:24:48 -0600

    Reads two environment variables, echoes message to specific channel

--- guild_bot/__main__.py
+++ guild_bot/__main__.py
@@ -1,13 +1,27 @@
 import discord
+import os
+import sys
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix=">")
+TOKEN = os.getenv('API_TOKEN')
+if TOKEN == None:
+    sys.exit("Environment variable API_TOKEN must be supplied")
 
+CHANNEL = os.getenv('SUGGESTION_CHANNEL')
+if CHANNEL == None:
+    sys.exit("Environment variable SUGGESTION_CHANNEL must be supplied")
+
+bot = commands.Bot(command_prefix=">")
 
 @bot.command()
 async def ping(ctx):
     await ctx.send("Hello World")
 
+@bot.command()
+async def suggest(ctx):
+    channel = bot.get_channel(int(CHANNEL))
+    msg = ctx.message.content
+    await channel.send(msg)
 
 # Need to generate token
-bot.run("")
+bot.run(TOKEN)
